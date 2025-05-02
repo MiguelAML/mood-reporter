6665138c5269b91da6f7f70d93f7b4c8bfecfc92
MiguelAML <miguel.ytmtz2008@gmail.com>
2025-05-02 13:16:09 -0400

    Update mood.js

--- mood.js
+++ mood.js
@@ -1,1 +1,28 @@
+// Mood Reporter
+// Your Name
+// Date
+
+let userName = "Miguel";
+let moodToday = "happy";
+
+function respondToMood(name, mood) {
+  console.log("Hi " + name + "!");
+
+  if (mood === "happy") {
+    console.log("That’s awesome. Keep it going!");
+  } else if (mood === "tired") {
+    console.log("Don’t forget to rest. You’ve earned it.");
+  } else if (mood === "frustrated") {
+    console.log("Take a breath. Better days are ahead.");
+  } else {
+    console.log("Thanks for checking in. Keep being you.");
+  }
+}
+
+respondToMood(userName, moodToday);
+
+for (let i = 0; i < 3; i++) {
+  console.log("You've got this, " + name + "!");
+}
+
 
